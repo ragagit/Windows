c381b13c7da8f2ce570fd03c4523e7d68ae25e18
raga <ragacan@ymail.com>
2017-08-17 11:54:58 -0400

    Adding UnitTest example.

---
+++ VSUnitTestC++/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,20 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "HelloWorld.h"
+
+int main()
+{
+	return 0;
+}
+
+int HelloWorld::getTwo()
+{
+	return 2;
+}
+
+int HelloWorld::getN() const
+{
+	return n;
+}
